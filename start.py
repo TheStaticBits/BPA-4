eb590b29d6f7bc0d34354bd2a0b3b012caa3cbb7
Stellarspeller <stellarspeller17@gmail.com>
2024-09-26 18:31:44 -0400

    initial setup stuff

--- start.py
+++ start.py
@@ -1,12 +1,45 @@
 import pygame
+import json
+
+with open("data/constants.json", "r") as file:
+    constants: dict = json.load(file)
+
 pygame.init()
 
 clock = pygame.time.Clock()
-FPS = 60 #relocate to a constants file soon
+FPS: int = 60 # relocate to a constants file soon
 
-while True:
-    clock.tick(FPS)
-    
+# Define screen size as 1200x675 (16:9 ratio)
+screen = pygame.display.set_mode(constants["windowSize"], pygame.RESIZABLE)
+pygame.display.set_caption("my game :]")
 
+running = True
+
+
+while running: # Everything happens here
+    clock.tick(constants["FPS"])
     
+    for event in pygame.event.get():
+
+        # Check for QUIT event
+        if event.type == pygame.QUIT:
+            running = False
+
+        # Check for window resize
+        elif event.type == pygame.VIDEORESIZE:
+            screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
+
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+            elif event.key == pygame.K_SPACE:
+                print(" you pressed space lmao   !")
+            elif event.key == pygame.K_BACKSPACE:
+                print("he he he haw")
+            elif event.key == pygame.K_RETURN:
+                print("heee heeee heeeee hawwwwwww!")
+            
+                
+    pygame.display.update() # END OF FRAME
+
 pygame.quit()
---
+++ util/vect.py
@@ -0,0 +1,36 @@
+class Vect:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Vect(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vect(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other):
+        return Vect(self.x * other, self.y * other)
+
+    def __truediv__(self, other):
+        return Vect(self.x / other, self.y / other)
+    
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+    
+    def getMagnitude(self):
+        return (self.x**2 + self.y**2)**0.5
+    
+    def squareClamp(self, max):
+        if self.x > max:
+            self.x = max
+        if self.y > max:
+            self.y = max
+        if self.x < -max:
+            self.x = -max
+        if self.y < -max:
+            self.y = -max
+
+    def circularClamp(self, radius=1):
+        if self.getMagnitude() > radius:
+            self = self * (radius / self.getMagnitude())
